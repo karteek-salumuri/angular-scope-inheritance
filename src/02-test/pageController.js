1ca7f18976d3285d21b9cde307d7f9e641df9197
ericdouglas <eric.eoop@gmail.com>
2014-11-22 15:22:07 -0200

    adding angular.js file =)

---
+++ src/02-test/pageController.js
@@ -0,0 +1,15 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module( 'guitarApp' )
+    .controller( 'PageController', PageController );
+
+  PageController.$inject = [ '$scope' ];
+
+  function PageController( $scope ) {
+
+  }
+  
+}());
